2ad31f6cf7d30958e24391bf975e3666f409dd18
Marcel Piaszczyk <marcel.piaszczyk@gmail.com>
2026-04-13 18:20:37 +0200

    add button for deleting notices

---
+++ app/components/DeleteNoticeButton.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { useState } from "react"
+import { useRouter } from "next/navigation"
+
+export function DeleteNoticeButton({id}: {id: string}) {
+
+    const [message, setMessage] = useState('');
+    const router = useRouter();
+
+    async function handleDelete() {
+
+        const response = await fetch (`api/notices/${id}`, {method: 'DELETE'});
+
+        if (response.ok){
+            router.refresh();
+        }
+
+        if (!response.ok){
+            setMessage('Wystąpił błąd podczas usuwania ogłoszenia')
+        }
+
+    }
+
+    return (
+        <>
+            <button onClick={handleDelete}>Usuń</button>
+            {message && <span>{message}</span>}
+        </>
+    )
+
+}
