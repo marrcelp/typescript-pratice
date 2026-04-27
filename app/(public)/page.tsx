1ae81a5b3fde96bda246a067a20893aca12b829c
Marcel Piaszczyk <marcel.piaszczyk@gmail.com>
2026-04-27 15:27:56 +0200

    create route for parking and events

---
+++ app/(public)/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage(){
+    return (
+        <main>
+            <h1>MallAssistant</h1>
+            <p>Witaj! Jestem asystentem centrum handlowego. Jak mogę Ci pomóc?</p>
+        </main>
+    )
+}
